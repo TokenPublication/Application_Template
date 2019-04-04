d6b78ebabe2cc603740b3596ca1002a778430be2
Berk <berk.dehrioglu@arcelik.com>
2019-04-04 16:20:19 +0300

    refactor: changes IDs of the fields and variables

--- app/src/main/java/com/tokeninc/sardis/application_template/ListMenuActivity.java
+++ app/src/main/java/com/tokeninc/sardis/application_template/ListMenuActivity.java
@@ -1,5 +1,6 @@
 package com.tokeninc.sardis.application_template;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -9,30 +10,31 @@ import android.view.View;
 
 public class  ListMenuActivity extends AppCompatActivity {
     private ListMenuAdapter listMenuAdapter;
-    private RecyclerView recyclerView;
+    private RecyclerView mRecyclerView;
     String[] productNames = {"Geleceği Yazanlar", "Paycell", "Tv+","Dergilik","Bip","GNC","Hesabım","Sim","LifeBox","Merhaba Umut","Yaani","Hayal Ortağım","Goller Cepte","Piri"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_menu);
 
-        recyclerView = findViewById(R.id.list_menu_rv);
+        mRecyclerView = findViewById(R.id.rv_list_menu);
 
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
 
-        recyclerView.setLayoutManager(linearLayoutManager);
-        recyclerView.setHasFixedSize(true);
+        mRecyclerView.setLayoutManager(linearLayoutManager);
+        mRecyclerView.setHasFixedSize(true);
         listMenuAdapter = new ListMenuAdapter(productNames);
         listMenuAdapter.setmClickListener(
                 new ListMenuAdapter.ClickListener() {
                     @Override
                     public void onItemClick(int position, View view) {
-                        Log.d("POSITION",String.valueOf(position));
-                        //Log.d("TEXT",String.valueOf();
+                        Intent intent = new Intent(ListMenuActivity.this,Main2Activity.class);
+                        startActivity(intent);
+
                     }
                 }
         );
-        recyclerView.setAdapter(listMenuAdapter);
+        mRecyclerView.setAdapter(listMenuAdapter);
 
     }
 }
