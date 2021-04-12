64b0a7d47ef13100f64661f450a523fd0a18de69
bariskarapinar <baris.karapinar@tokeninc.com>
2021-04-12 18:16:47 +0300

    Bundle data added

--- app/src/main/java/com/tokeninc/sardis/application_template/UI/Activities/DummySaleActivity.java
+++ app/src/main/java/com/tokeninc/sardis/application_template/UI/Activities/DummySaleActivity.java
@@ -118,7 +118,7 @@ public class DummySaleActivity extends BaseActivity implements View.OnClickListe
     private SampleReceipt getSampleReceipt(String cardNo, String ownerName) {
         SampleReceipt receipt = new SampleReceipt();
         receipt.setMerchantName("TOKEN FINTECH");
-        receipt.setMerchantID("c");
+        receipt.setMerchantID("26854222228");
         receipt.setPosID("000002AC");
         receipt.setCardNo(cardNo);
         receipt.setFullName(ownerName);
